6c1ff187ff20850ae4bcc66ff14c969b3fa8a6e8
Michele Vaccari <michele.vaccari@outlook.com>
2022-12-18 15:13:51 +0100

    Add greedy nearest neighbor

--- src/tsppdcli.py
+++ src/tsppdcli.py
@@ -5,6 +5,7 @@ from tsppd.solver.oneilHoffmanEnumerator import OneilHoffmanEnumerator
 from tsppd.solver.greedyTemplate import GreedyTemplate
 from tsppd.solver.greedyPickupFirst import GreedyPickupFirst
 from tsppd.solver.greedyRequestOrder import GreedyRequestOrder
+from tsppd.solver.greedyNearestNeighbor import GreedyNearestNeighbor
 import click
 
 @click.group()
@@ -30,7 +31,7 @@ cli.add_command(generate_instance)
 
 @click.command()
 @click.option('--input-instance-path', required=True, default=None, help='Path where read the instance in JSON format.', type = click.STRING)
-@click.option('--solver-method', required=True, default=None, help='Choose the solver method to use.', type=click.Choice(['brute-force-enumerator', 'oneil-hoffman-enumerator', 'greedy-pickup-first', 'greedy-request-order'], case_sensitive=False))
+@click.option('--solver-method', required=True, default=None, help='Choose the solver method to use.', type=click.Choice(['brute-force-enumerator', 'oneil-hoffman-enumerator', 'greedy-pickup-first', 'greedy-request-order', 'greedy-nearest-neighbor'], case_sensitive=False))
 def solve(input_instance_path, solver_method):
     """Solve an instance of the tsppd problem."""
     print("\n\nREADED INSTANCE\n\n")
@@ -59,6 +60,12 @@ def solve(input_instance_path, solver_method):
         solution = greedyTemplate.calculate_solution()
         print("\nGREEDY REQUEST-ORDER SOLUTION: \n")
         print(solution)
+    elif (solver_method == "greedy-nearest-neighbor"):
+        greedyNearestNeighbor = GreedyNearestNeighbor()
+        greedyTemplate = GreedyTemplate(instance, greedyNearestNeighbor)
+        solution = greedyTemplate.calculate_solution()
+        print("\nGREEDY NEAREST-NEIGHBOR SOLUTION: \n")
+        print(solution)
 
 cli.add_command(solve)
 
@@ -72,10 +79,13 @@ if __name__ == '__main__':
     # tsppdcli.py generate-instance --requests 10 --output-instance-path C:\dev\TSP-con-pick-up-and-delivery\src\instances\10-request-weights-random.json
     # tsppdcli.py generate-instance --requests 10 --weights-random --output-instance-path C:\dev\TSP-con-pick-up-and-delivery\src\instances\10-request-weights-random.json
     # tsppdcli.py generate-instance --requests 10 --weights-as-euclidean-distance --output-instance-path C:\dev\TSP-con-pick-up-and-delivery\src\instances\10-request-weights-euclidean-distance.json
+
     # tsppdcli.py solve --input-instance-path C:\dev\TSP-con-pick-up-and-delivery\src\instances\2-request-weights-random.json --solver-method brute-force-enumerator
     # tsppdcli.py solve --input-instance-path C:\dev\TSP-con-pick-up-and-delivery\src\instances\2-request-weights-random.json --solver-method oneil-hoffman-enumerator
+    
     # tsppdcli.py solve --input-instance-path C:\dev\TSP-con-pick-up-and-delivery\src\instances\2-request-weights-random.json --solver-method greedy-pickup-first
     # tsppdcli.py solve --input-instance-path C:\dev\TSP-con-pick-up-and-delivery\src\instances\2-request-weights-random.json --solver-method greedy-request-order
+    # tsppdcli.py solve --input-instance-path C:\dev\TSP-con-pick-up-and-delivery\src\instances\2-request-weights-random.json --solver-method greedy-nearest-neighbor
 
     # TODO
     # tsppdcli.py solve --input-instance-path C:\dev\TSP-con-pick-up-and-delivery\src\instances\2-request-weights-random.json --solver-method oneil-hoffman-enumerator --statistics
